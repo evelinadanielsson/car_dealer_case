5a84eb2efb786a67d505195ff4cf32111b308d90
evelinadanielsson <evelina.danielsson@gmail.com>
2020-12-12 10:27:30 +0100

    added dev-dependecy and some files

--- scripts/dbenterdata.js
+++ scripts/dbenterdata.js
@@ -3,25 +3,26 @@ var dbconfig = require('../config/database');
 var jsonData = require('../config/data.json');
 
 
-var carshop = jsonData.carshop;
-
+var employees = jsonData.carshop.employees;
+var carmodels= jsonData.carshop.carmodels;
+var sales = jsonData.carshop.sales;
 
 
 var connection = mysql.createConnection(dbconfig.connection);
 
-carshop.employees.forEach(element => {
+employees.forEach(element => {
     connection.query('INSERT INTO `' + dbconfig.database + '`.`employees` (`name`) \
     VALUES("' + element.name + '");'
   );
 });
 
-carshop.carmodels.forEach(element => {
+carmodels.forEach(element => {
     connection.query('INSERT INTO `' + dbconfig.database + '`.`carmodels` (`brand`, `model`, `price`) \
     VALUES("' + element.brand + '", "' + element.brand + '", ' + element.price + ');'
   );
 });
 
-carshop.sales.forEach(element => {
+sales.forEach(element => {
     connection.query('INSERT INTO `' + dbconfig.database + '`.`sales` (`employee_id`, `carmodel_id`) \
     VALUES(' + element.employee_id + ', ' + element.carmodel_id + ');'
   );
